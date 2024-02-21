17223806ab805d2ee015a59cfec915af7d6023b1
Alcides Paz <alcidesnp@hotmail.com>
2024-02-21 12:48:50 -0300

    Adicionado endereço URL API AZURE

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8090/",
+    // baseURL: "http://localhost:8090/",
+    baseURL: "https://api-ecosys.azurewebsites.net/",
 })
 
 export default api
